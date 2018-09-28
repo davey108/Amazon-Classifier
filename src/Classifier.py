b2c7363ae436aad7c89c1deac18241fbfee5a2a5
davey108 <sligar.david@gmail.com>
2018-09-28 18:40:26 -0400

    Correct code

--- src/Classifier.py
+++ src/Classifier.py
@@ -2,67 +2,42 @@ import numpy
 import re
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import time
 import threading
 
 result_arr = []
-# numpy.seterr(invalid='ignore')
-# def cosine_similarity(vector1, vector2):
-#    dot_product = numpy.dot(vector1.toarray(), vector2.toarray())
-#    # calculate the norm aka magnitude
-#    vect1_magnitude = numpy.linalg.norm(vector1)
-#    vect2_magnitude = numpy.linalg.norm(vector2)
-#    return dot_product/(vect1_magnitude*vect2_magnitude)
-
-
-def cosine_similarity(vector_1, vector_2):
-    vector_1 = vector_1.toarray()
-    vector_2 = vector_2.toarray()
-    return numpy.dot(vector_1, vector_2.T) / \
-        (numpy.sqrt(numpy.dot(vector_1, vector_1.T)) * numpy.sqrt(numpy.dot(vector_2, vector_2.T)))
 
 
 # test_review is each row of the test_matrix
-def knn(train_data, test_review, rating_arr, k):
+def knn(distance_list, rating_arr, k):
+    # threadLock.acquire()
     collected_distances = []
-    train_rows = numpy.shape(train_matrix)[0]
-    # get the cosine similarity of each review to each of the review in train_set
-    for num_row in range(train_rows):
-        similarity_dist = cosine_similarity(train_data.getrow(num_row), test_review)
-        collected_distances.append((rating_arr[num_row], similarity_dist))
-    collected_distances = sorted(collected_distances, key=distance, reverse=True)
+    i = 0
+    for item in distance_list:
+        collected_distances.append((rating_arr[i], item))
+        i += 1
+    collected_distances = sorted(collected_distances, key=lambda x: x[1], reverse=True)
     limited_dist_list = collected_distances[0:k]
     res = Counter(elem[0] for elem in limited_dist_list)
-    print(res.most_common(2))
+    # threadLock.release()
     return res.most_common(1)[0][0]
-    # get values of k neighbors
-    # count_neg = 0
-    # count_pos = 0
-    # for i in range(k):
-    #     if collected_distances[i][0] == "+1":
-    #         count_pos += 1
-    #     else:
-    #         count_neg += 1
-    # if count_pos >= count_neg:
-    #     return "+1"
-    # else:
-    #     return "-1"
-
-
-def distance(element):
-    return element[1]
 
 
 # Should return an array of knn results along with the thread id
 def start_knn(train_m, test_m, ratings_ls, k, begin, end):
     rating_res = []
     for i in range(begin, end):
-        rating = knn(train_m, test_m.getrow(i), ratings_ls, k)
+        print("Review: " + str(i) + "\n")
+        similarity_list = cosine_similarity(test_m.getrow(i), train_m).tolist()
+        rating = knn(similarity_list[0], ratings_ls, k)
         rating_res.append(rating)
-    result_arr.append((threading.current_thread().name, result_arr))
+    result_arr.append((threading.current_thread().name, rating_res))
 
 
 def start_threads(train_m, test_m, rating_ls, k):
     global result_arr
+    global empty_review_index
     rows_test = numpy.shape(test_matrix)[0]
     threads = []
     n = 4
@@ -82,21 +57,28 @@ def start_threads(train_m, test_m, rating_ls, k):
 
     for t in threads:
         t.join()
-    result_file = open("result.txt", "w")
+
     result_arr = sorted(result_arr, key=lambda x: x[0])
     # layout of result arr = [(thread_id,knn_result),(thread_id,knn_result),...]
+    result_list = []
     for item in result_arr:
         for rating in item[1]:
-            result_file.write(rating)
+            # result_file.write(rating + "\n")
+            result_list.append(rating)
+    for index in range(len(empty_review_index)):
+        result_list.insert(empty_review_index[index], "-1")
+    result_file = open("result.txt", "w")
+    for element in result_list:
+        result_file.write(element + "\n")
     result_file.close()
 
 
+start_time = time.time()
 train_file = open("train/train.data", "r")
 train_corpus = []
 rating_labels = []
 for review in train_file:
     text_line = review.split("\t")
-    # review
     review = text_line[1]
     # rid of all the numbers and special symbols
     new_review = re.sub("[^a-zA-Z_' -]", "", review)
@@ -113,29 +95,23 @@ train_matrix = tfidf_vectorizer.fit_transform(train_corpus)
 # parse the test data
 test_file = open("test/test.data", "r")
 test_corpus = []
+# uses to stores index of empty review which is to be negative
+empty_review_index = []
+count_index = 0
 for review in test_file:
     new_review = re.sub("[^a-zA-Z_' -]", "", review)
     # for empty reviews
     if new_review != "":
         test_corpus.append(new_review)
+    else:
+        empty_review_index.append(count_index)
+    count_index += 1
 test_file.close()
 
 # Now transform the test data into a term document matrix using the learned vocabulary and idf from the train_data
 test_matrix = tfidf_vectorizer.transform(test_corpus)
-test_rows = numpy.shape(test_matrix)[0]
-
-# Make the ranges for n threads
-
-# result_file = open("result.txt", "w")
-# for row in range(test_rows):
-#     rating = knn(train_matrix, test_matrix.getrow(row), rating_labels, 130)
-#     print(str(row) + ":" + rating)
-#     result_file.write(rating)
-# result_file.close()
-
-start_threads(train_matrix, test_matrix, rating_labels, 100)
-
-
-
+start_threads(train_matrix, test_matrix, rating_labels, 130)
+# test_rows = numpy.shape(test_matrix)[0]
+print("--------------%s minutes----------------" % ((time.time() - start_time)//60))
 
 
